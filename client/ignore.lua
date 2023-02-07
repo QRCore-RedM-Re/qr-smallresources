40f7f29091f28a7febc1b6ad5cd01518566cf88a
Molicheu <64894726+Molicheu@users.noreply.github.com>
2023-02-07 00:21:02 -0500

    Optimize code

--- client/ignore.lua
+++ client/ignore.lua
@@ -1,59 +1,49 @@
+local function deleteEntity(entity, blacklistedModels)
+    local model = GetEntityModel(entity)
+    if not blacklistedModels[model] then return end
+    SetEntityAsMissionEntity(entity, true, true)
+    DeleteEntity(entity)
+    SetEntityAsNoLongerNeeded(entity)
+end
+
+local function handleBlacklistedEntities(entityType, blacklistedModels)
+    local pool = GetGamePool(entityType)
+    for i = 1, #pool do
+        deleteEntity(pool[i], blacklistedModels)
+    end
+end
+
 Citizen.CreateThread(function()
     while true do
-        for _, sctyp in next, Config.BlacklistedScenarios['TYPES'] do
-            SetScenarioTypeEnabled(sctyp, false)
+        for i = 1, #Config.BlacklistedScenarios['TYPES'] do
+            SetScenarioTypeEnabled(Config.BlacklistedScenarios['TYPES'][i], false)
         end
-        for _, scgrp in next, Config.BlacklistedScenarios['GROUPS'] do
-            SetScenarioGroupEnabled(scgrp, false)
+        for i = 1, #Config.BlacklistedScenarios['GROUPS'] do
+            SetScenarioGroupEnabled(Config.BlacklistedScenarios['GROUPS'][i], false)
         end
-		Citizen.Wait(10000)
+        Citizen.Wait(10000)
     end
 end)
 
 Citizen.CreateThread(function()
     while true do
-        local vehiclePool = GetGamePool('CVehicle')
-		local pedPool = GetGamePool('CPed')
-		local objectPool = GetGamePool('CObject')
-        for k,v in pairs(vehiclePool) do
-            if Config.BlacklistedVehicles[GetEntityModel(v)] then
-				SetEntityAsMissionEntity(v, true, true)
-                DeleteVehicle(v)
-				SetEntityAsNoLongerNeeded(v)
-            end
-        end
-		for k,v in pairs(pedPool) do
-			SetPedDropsWeaponsWhenDead(v, false)
-			if Config.BlacklistedPeds[GetEntityModel(v)] then
-				SetEntityAsMissionEntity(v, true, true)
-				DeletePed(v)
-				SetEntityAsNoLongerNeeded(v)
-			end
-		end
-		for k,v in pairs(objectPool) do
-			if Config.BlacklistedObjects[GetEntityModel(v)] then
-				SetEntityAsMissionEntity(v, true, true)
-				DeleteObject(v)
-				SetEntityAsNoLongerNeeded(v)
-			end
-		end
+        handleBlacklistedEntities("CVehicle", Config.BlacklistedVehicles)
+        handleBlacklistedEntities("CPed", Config.BlacklistedPeds)
+        handleBlacklistedEntities("CObject", Config.BlacklistedObjects)
         Citizen.Wait(250)
     end
 end)
 
--- removes events and challenge notfications
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(10)
-        local size = GetNumberOfEvents(0)   
-        if size > 0 then
-            for i = 0, size - 1 do
-                local eventAtIndex = GetEventAtIndex(0, i)
-                if eventAtIndex == GetHashKey("EVENT_CHALLENGE_GOAL_COMPLETE") or eventAtIndex == GetHashKey("EVENT_CHALLENGE_REWARD") or eventAtIndex == GetHashKey("EVENT_DAILY_CHALLENGE_STREAK_COMPLETED") then 
-                    Citizen.InvokeNative(0x6035E8FBCA32AC5E)
-                end
+        local size = GetNumberOfEvents(0)
+        if size == 0 then return end
+        for i = 0, size - 1 do
+            local eventAtIndex = GetEventAtIndex(0, i)
+            if eventAtIndex == joaat("EVENT_CHALLENGE_GOAL_COMPLETE") or eventAtIndex == joaat("EVENT_CHALLENGE_REWARD") or eventAtIndex == joaat("EVENT_DAILY_CHALLENGE_STREAK_COMPLETED") then
+                Citizen.InvokeNative(0x6035E8FBCA32AC5E)
             end
         end
     end
 end)
-

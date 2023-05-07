b3ea1986beb0e78289286c0b955eca9da36ae048
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2023-05-07 13:59:05 -0600

    refactor(client): stuck horse onesync fix and hide events/challenge notify

--- client/ignore.lua
+++ client/ignore.lua
@@ -1,4 +1,8 @@
-Citizen.CreateThread(function()
+QRCore = exports['qr-core']:GetCoreObject()
+Keys = QRCore.Shared.Keybinds
+
+-- Blacklisted Scenarios --
+CreateThread(function()
     while true do
         for _, sctyp in next, Config.BlacklistedScenarios['TYPES'] do
             SetScenarioTypeEnabled(sctyp, false)
@@ -6,54 +10,38 @@ Citizen.CreateThread(function()
         for _, scgrp in next, Config.BlacklistedScenarios['GROUPS'] do
             SetScenarioGroupEnabled(scgrp, false)
         end
-		Citizen.Wait(10000)
+		Wait(10000)
     end
 end)
 
-Citizen.CreateThread(function()
-    while true do
-        local vehiclePool = GetGamePool('CVehicle')
-		local pedPool = GetGamePool('CPed')
-		local objectPool = GetGamePool('CObject')
-        for k,v in pairs(vehiclePool) do
-            if Config.BlacklistedVehicles[GetEntityModel(v)] then
-				SetEntityAsMissionEntity(v, true, true)
-                DeleteVehicle(v)
-				SetEntityAsNoLongerNeeded(v)
-            end
-        end
-		for k,v in pairs(pedPool) do
-			SetPedDropsWeaponsWhenDead(v, false)
-			if Config.BlacklistedPeds[GetEntityModel(v)] then
-				SetEntityAsMissionEntity(v, true, true)
-				DeletePed(v)
-				SetEntityAsNoLongerNeeded(v)
-			end
-		end
-		for k,v in pairs(objectPool) do
-			if Config.BlacklistedObjects[GetEntityModel(v)] then
-				SetEntityAsMissionEntity(v, true, true)
-				DeleteObject(v)
-				SetEntityAsNoLongerNeeded(v)
-			end
-		end
-        Citizen.Wait(250)
-    end
-end)
-
--- removes events and challenge notfications
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(10)
-        local size = GetNumberOfEvents(0)   
-        if size > 0 then
-            for i = 0, size - 1 do
-                local eventAtIndex = GetEventAtIndex(0, i)
-                if eventAtIndex == GetHashKey("EVENT_CHALLENGE_GOAL_COMPLETE") or eventAtIndex == GetHashKey("EVENT_CHALLENGE_REWARD") or eventAtIndex == GetHashKey("EVENT_DAILY_CHALLENGE_STREAK_COMPLETED") then 
-                    Citizen.InvokeNative(0x6035E8FBCA32AC5E)
+-- Remove Frozen Carriages/Horses (OneSync Fix) --
+CreateThread(function()
+    while true do Wait(10)
+        local Pool = GetGamePool('CVehicle')
+        for i = 1, #Pool do
+            Wait(10)
+            local Veh = IsEntityAVehicle(Pool[i])
+            local Stopped = IsVehicleStopped(Pool[i])
+            local Horse = Citizen.InvokeNative(0xA8BA0BAE0173457B, Pool[i], 0)
+            local Driver = Citizen.InvokeNative(0x2963B5C1637E8A27, Pool[i])
+            local Walking = IsPedWalking(Horse)
+            if Veh and Stopped then
+                if Walking then
+                    if Driver ~= PlayerPedId() then
+                        if Driver then DeleteEntity(Driver) end
+                        DeleteEntity(Pool[i])
+                    end
                 end
             end
         end
+        Wait(Config.StuckWagons * 1000)
     end
 end)
 
+-- Remove Events and Challenge Notifications --
+AddEventHandler('events:listener', function(name, _)
+    if name == 'EVENT_CHALLENGE_GOAL_COMPLETE' or name == "EVENT_CHALLENGE_REWARD" or name == "EVENT_DAILY_CHALLENGE_STREAK_COMPLETED"  then
+        print('Hiding Events and Challenge Notify')
+        Citizen.InvokeNative(0x6035E8FBCA32AC5E) -- UiFeedClearAllChannels // Hides Toast Notifications
+    end
+end)

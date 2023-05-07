1e06b432ee432a971901ceb2eea4e234b8ab67ca
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2023-05-07 14:11:07 -0600

    refactor(pvp): toggle PVP

--- client/pvp.lua
+++ client/pvp.lua
@@ -1,8 +1,40 @@
-Citizen.CreateThread(function()
+local PVP, RELATION = true, 5
+local shown = false
+CreateThread(function()
     while true do
-        Citizen.Wait(1)
-        Citizen.InvokeNative(0xF808475FA571D823, true) --enable friendly fire
-        NetworkSetFriendlyFireOption(true)
-        SetRelationshipBetweenGroups(5, `PLAYER`, `PLAYER`)
+        Wait(1)
+
+        if GetRelationshipBetweenGroups(`PLAYER`, `PLAYER`) ~= RELATION then
+            NetworkSetFriendlyFireOption(PVP)
+            SetRelationshipBetweenGroups(RELATION, `PLAYER`, `PLAYER`)
+        end
+
+        if Config.TogglePVP then
+            if IsControlJustReleased(0, Keys['8']) then
+                PVP = not PVP
+                if RELATION == 5 then
+                    RELATION = 1 -- Respect
+                else
+                    RELATION = 5 -- Hate
+                end
+            end
+
+            if not PVP then
+                if not shown then
+                    lib.showTextUI('PVP DISABLED', {
+                        position = "top-center",
+                        icon = 'skull',
+                        style = {
+                            borderRadius = 0,
+                            backgroundColor = '#000000',
+                            color = 'white'
+                        }
+                    })
+                    shown = true
+                end
+            else
+                if shown then lib.hideTextUI() shown = false end
+            end
+        end
     end
 end)
